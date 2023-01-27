084220004da58bbeb71ff39c7cff702e5103c5df
Lygia Ferronato Bueno <lybueno@gmail.com>
2023-01-27 16:41:13 -0300

    Swagger file and configurations

---
+++ src/main/java/io/github/lybueno/attornatus/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package io.github.lybueno.attornatus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.github.lybueno.attornatus.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Person and Address API")
+                .description("Documentation of the API developed for the technical evaluation of the company " +
+                        "Attornatus.")
+                .version("1.0")
+                .build();
+    }
+}
